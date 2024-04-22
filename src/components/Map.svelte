17c494a1e215b2d8c649d4cb4bda4e197fe9e64b
Yurith Rubio <iamyurith@gmail.com>
2024-04-22 12:36:50 +0200

    paddings

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -126,7 +126,7 @@
 </script>
 
 
-<div class="colors-wrapper">
+<div class="colors-wrapper padding-sides padding-sides">
   <div class="column1">
     <p class="support">Pro Russia</p>
     <p class="support">Russia Leaning</p>
@@ -146,7 +146,7 @@
   <svg id="my_dataviz" preserveAspectRatio="xMinYMin meet"></svg>
 </div>
 
-<p data-caps="initial" class="article__body-text article__body-text--dropcap">By invading ukraine, Vladimir Putin has divided the world. The West and its allies have presented a rare unified front against the Russian president’s attack. NATO is enjoying a surge of support within its member countries (<a href="/graphic-detail/2022/03/23/putins-aggression-has-bolstered-support-for-nato" data-analytics="in_body:link_1:para_1">and wannabe joiners</a>). The EU has projected the role of a <a href="/europe/the-eus-unity-over-ukraine-has-given-it-surprising-heft/21808306" data-analytics="in_body:link_2:para_1">first-rate power</a>. And co-ordinated efforts, including sanctions and banking restrictions, have punished Russia’s economy, <a href="/finance-and-economics/2022/03/30/under-unprecedented-sanctions-how-is-the-russian-economy-faring" data-analytics="in_body:link_3:para_1">at least in the short term</a>. But from other countries Russia still enjoys some support. The <a href="https://www.eiu.com/n/russia-can-count-on-support-from-many-developing-countries/" data-analytics="in_body:link_4:para_1">Economist Intelligence Unit</a>, our sister company, has measured government actions globally since the war broke out, and countries’ historical ties with Russia, to divide the world into three broad categories: governments that are West-leaning, Russia-leaning and neutral amid <a href="/briefing/2022/04/02/what-next-for-russia" data-analytics="in_body:link_5:para_1">the conflict</a>.
+<p data-caps="initial" class="article__body-text article__body-text--dropcap padding-sides">By invading ukraine, Vladimir Putin has divided the world. The West and its allies have presented a rare unified front against the Russian president’s attack. NATO is enjoying a surge of support within its member countries (<a href="/graphic-detail/2022/03/23/putins-aggression-has-bolstered-support-for-nato" data-analytics="in_body:link_1:para_1">and wannabe joiners</a>). The EU has projected the role of a <a href="/europe/the-eus-unity-over-ukraine-has-given-it-surprising-heft/21808306" data-analytics="in_body:link_2:para_1">first-rate power</a>. And co-ordinated efforts, including sanctions and banking restrictions, have punished Russia’s economy, <a href="/finance-and-economics/2022/03/30/under-unprecedented-sanctions-how-is-the-russian-economy-faring" data-analytics="in_body:link_3:para_1">at least in the short term</a>. But from other countries Russia still enjoys some support. The <a href="https://www.eiu.com/n/russia-can-count-on-support-from-many-developing-countries/" data-analytics="in_body:link_4:para_1">Economist Intelligence Unit</a>, our sister company, has measured government actions globally since the war broke out, and countries’ historical ties with Russia, to divide the world into three broad categories: governments that are West-leaning, Russia-leaning and neutral amid <a href="/briefing/2022/04/02/what-next-for-russia" data-analytics="in_body:link_5:para_1">the conflict</a>.
 </p>
 
 <style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,16 @@
     import Map from '../components/Map.svelte';
 </script>
 
-<h1>Russia support around the globe</h1>
+<h1 class="padding-sides">Russia support around the globe</h1>
 
 <Map />
 
-<p class="article__body-text">
+<p class="article__body-text padding-sides">
 <a href="https://www.economist.com/graphic-detail/2023/03/31/who-are-russias-supporters" alt="who-are-russias-supporters">Source of information</a>
 </p>
 
-<p class="article__body-text">Original article from <a href="https://informationisbeautiful.net/visualizations/ukraine-russian-war-infographics-data-visuals/#two-years" alt="ukraine-russian-war-infographics-data-visuals">source</a>. Design and development by Yurith Rubio</p>
+<p class="article__body-text padding-sides">Original article from <a href="https://informationisbeautiful.net/visualizations/ukraine-russian-war-infographics-data-visuals/#two-years" alt="ukraine-russian-war-infographics-data-visuals">source</a>. Design and development by Yurith Rubio</p>
+
 <style>
     h1 {
         color: #07203f;
